96230f79d3cace018ad1b45d2c0c0777292eadce
Nabil ngouhouo <nabilf045@gmail.com>
2025-05-15 18:55:17 +0100

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -54,6 +54,12 @@ exclude_patterns = []
 # a list of builtin themes.
 
 html_theme = 'alabaster'
+# docs/source/conf.py
+html_theme = 'furo'
+html_theme_options = {  # Configuration optionnelle
+    "navigation_depth": 4,
+    "top_of_page_button": "edit",
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
